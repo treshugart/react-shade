a5cd2c5e915e9a74b47f8bb5c228b752f4f14ee8
Trey Shugart <treshugart@gmail.com>
2018-01-18 18:11:19 +1100

    Fix events not bubbling through shadow boundaries for react's event system.

--- src/Root.js
+++ src/Root.js
@@ -3,6 +3,7 @@
 import React, { Component, type Node } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
+import retargetEvents from 'react-shadow-dom-retarget-events';
 
 type Props = {
   children?: Node,
@@ -29,6 +30,7 @@ export class Root extends Component<Props, State> {
       shadowRoot.appendChild(
         document.createElement(this.props.tagForShadowRoot)
       );
+      retargetEvents(shadowRoot);
       this.setState({ shadowRoot });
     }
   };
