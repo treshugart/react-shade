c934b67b1e649674788b5dfc63cc850e2507c672
Trey Shugart <treshugart@gmail.com>
2018-08-28 10:18:29 +1000

    Support limited automatic css scoping in non-native environments.

--- src/Style.js
+++ src/Style.js
@@ -1,6 +1,8 @@
 // @flow
 
 import React from "react";
+import shadowCss from "shadow-css";
+import Context from "./Context";
 import { styleRules } from "./internal/css";
 import type { StyleRules } from "./internal/types";
 
@@ -9,5 +11,11 @@ type Props = {
 };
 
 export const Style = ({ children, ...props }: Props) => (
-  <style>{styleRules(children || {}, props)}</style>
+  <Context.Consumer>
+    {root => {
+      const unscopedCss = styleRules(children, props);
+      const scopedCss = shadowCss(unscopedCss)(root && root.host);
+      return scopedCss ? <style>{scopedCss}</style> : "";
+    }}
+  </Context.Consumer>
 );
