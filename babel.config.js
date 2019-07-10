7d94e5f5a37f7f8bae4286316e034151b99c26b8
Trey Shugart <treshugart@gmail.com>
2019-07-10 13:50:23 +1000

    Get tests passing.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  presets: ["next/babel"]
+const envs = {
+  next: {
+    presets: ["next/babel"]
+  },
+  test: {
+    presets: ["next/babel"]
+  }
+};
+
+module.exports = envs[process.env.NODE_ENV] || {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-typescript"
+  ]
 };
